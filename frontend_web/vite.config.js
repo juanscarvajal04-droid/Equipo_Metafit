40692e7262df80ebb3c59c8d78c905a0d3a28c2c
juanscarvajal04-droid <juanscarvajal.04@gmail.com>
2026-08-09 19:57:37 -0500

    seguridad prod: ofuscación terser (drop_console/debugger, mangle, sin comentarios), sourcemaps off, cssMinify, CSP meta y bloqueo de clic derecho fuera de localhost

--- frontend_web/vite.config.js
+++ frontend_web/vite.config.js
@@ -5,6 +5,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: { drop_console: true, drop_debugger: true },
+      mangle: true,
+      output: { comments: false },
+    },
+    sourcemap: false,
+    cssMinify: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
