5360745156e5f6886ce266ce51cb26dc5e9fb323
isabella198913 <isabellacaldasperez@gmail.com>
2026-09-20 22:42:02 -0500

    Add files via upload

---
+++ Movil/App.js
@@ -0,0 +1,26 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { AuthProvider } from './src/context/AuthContext';
+import { ThemeProvider, useTheme } from './src/context/ThemeContext';
+import AppNavigator from './src/navigation/AppNavigator';
+
+function Root() {
+  const { isDark } = useTheme();
+  return (
+    <>
+      <StatusBar style={isDark ? 'light' : 'dark'} />
+      <AppNavigator />
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <Root />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
